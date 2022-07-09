0284b32a03f4feeee3566bbf92484a45ee93053a
haerulmutakin <haerulm.takin@gmail.com>
2022-07-09 11:38:10 +0700

    remove strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import './stylesheets/style.scss';
+import Main from './Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
-);
+root.render( <Main />);
