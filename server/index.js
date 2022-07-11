e929f66f91c8f7dc7474936f0fe7684d6f9b60e9
haerulmutakin <haerulm.takin@gmail.com>
2022-07-11 13:31:58 +0700

    listening like event form client

--- server/index.js
+++ server/index.js
@@ -16,10 +16,32 @@ const socketIo = new Server(httpServer, {
     }
 });
 
+const onlineUsers = [];
+
+const addOnlineUser = (username, socketId) => {
+    const userIndex = onlineUsers.findIndex(item => item.username === username);
+    if(userIndex >= 0) {
+        onlineUsers[userIndex] = {username: username, socketId: socketId}
+    } else {
+        onlineUsers.push({username: username, socketId: socketId})
+    }
+}
+
+const getUser = (username) => {
+    return onlineUsers.find(item => item.username === username)
+}
+
 socketIo.on('connection', (socket) => {
 
-    socket.on('jr', (data) => {
-        socket.join(data);
+    socket.on('newOnlineUser', (data) => {
+        addOnlineUser(data.username, socket.id);
+    })
+
+    socket.on('like', ({receiver, sender}) => {
+        const user = getUser(receiver);
+        if(user) {
+            socket.to(user.socketId).emit('like', `${sender} likes your quote`)
+        }
     })
 
     socket.on('sm', (data) => {
