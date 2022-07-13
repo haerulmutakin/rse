43a3e6173985a9b1bbaf9decf9a8c5b4be83f20e
haerulmutakin <haerulm.takin@gmail.com>
2022-07-13 16:10:38 +0700

    feature new quote

---
+++ client/src/components/NewQuote.js
@@ -0,0 +1,49 @@
+import { useContext, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight, faPalette, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import QuoteColorConstant from '../_constant/QuoteColor.constant';
+import UserContext from '../_context/UserContext';
+import Api from "../_api/ApiInstance";
+import { useNavigate } from 'react-router-dom';
+
+const NewQuote = () => {
+    const navigate = useNavigate();
+    const user = useContext(UserContext);
+    const [color, setColor] = useState(0);
+    const [quote, setQuote] = useState('');
+
+    const handlePaletteClick = () => {
+        if(color < QuoteColorConstant.length) {
+            setColor(color + 1);
+        } else {
+            setColor(0)
+        }
+    }
+
+    const handleSubmit = async () => {
+        const payload = {
+            theme: QuoteColorConstant[color],
+            body: quote,
+            userId: user._id
+        }
+
+        const resp = await Api.post('/quote', payload)
+        const data = resp.data;
+        if(data.status === true) {
+            navigate(-1);
+        } else {
+            console.log('gagal tambah quote')
+        }
+    }
+    return ( 
+        <div className="add-quote mx-5 mt-5">
+            <div className="area-container" style={{backgroundColor: QuoteColorConstant[color]}}>
+                <textarea onChange={(e) => setQuote(e.target.value)}></textarea>
+            </div>
+            <button onClick={handlePaletteClick} className='color-palette'><FontAwesomeIcon icon={faPalette} /></button>
+            <button onClick={handleSubmit} className='submit-btn'><FontAwesomeIcon icon={faAngleDoubleRight} /></button>
+        </div>
+     );
+}
+ 
+export default NewQuote;
