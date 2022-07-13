3eed02c26516b4de325a45b1efdd3b115e0f2e01
haerulmutakin <haerulm.takin@gmail.com>
2022-07-13 20:16:17 +0700

    sort quote by createdAt

--- server/controllers/quote.controller.js
+++ server/controllers/quote.controller.js
@@ -2,7 +2,9 @@ const ResponseSchema = require('../_utils/response-body.util');
 const Quote = require('../models/quote.model');
 
 exports.findQuotes = async (req, res) => {
-    const quotes = await Quote.find().populate('userId', 'username');
+    const quotes = await Quote.find()
+                            .sort({'createdAt': 'desc'})
+                            .populate('userId', 'username');
     const result = [];
     quotes.forEach(item => {
         const {_id, username } = item.userId;
