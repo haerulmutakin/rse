1f5c2f7cf316553ad77ce33a6304e37451a17fea
haerulmutakin <haerulm.takin@gmail.com>
2022-07-13 21:51:45 +0700

    add socket io connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const {Server} = require('socket.io');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -17,6 +19,47 @@ App.use(parser.urlencoded({extended: true}));
 
 App.use('/api/v1', appRoutes)
 
+const httpServer = http.createServer(App);
+
+
+
+const socketIo = new Server(httpServer,  {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+
+const onlineUsers = [];
+
+const addOnlineUser = (username, socketId) => {
+    const userIndex = onlineUsers.findIndex(item => item.username === username);
+    if(userIndex >= 0) {
+        onlineUsers[userIndex] = {username: username, socketId: socketId}
+    } else {
+        onlineUsers.push({username: username, socketId: socketId})
+    }
+}
+
+const getUser = (username) => {
+    return onlineUsers.find(item => item.username === username)
+}
+
+socketIo.on('connection', (socket) => {
+
+    socket.on('newOnlineUser', (data) => {
+        addOnlineUser(data.username, socket.id);
+    })
+
+    socket.on('like', ({receiver, sender}) => {
+        const user = getUser(receiver);
+        if(user) {
+            socket.to(user.socketId).emit('like', `${sender} likes your quote`)
+        }
+    })
+})
+
 mongoose.connect('mongodb://localhost:27017/react-socket-node', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -28,6 +71,6 @@ mongoose.connect('mongodb://localhost:27017/react-socket-node', {
     }
 })
 
-App.listen(3001, () => {
+httpServer.listen(3001, () => {
     console.log('App is running')
 })
