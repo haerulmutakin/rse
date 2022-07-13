99728d8f41726e1402395d5cfd3dc43ae94e965e
haerulmutakin <haerulm.takin@gmail.com>
2022-07-13 15:05:00 +0700

    get comments from api

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,21 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Api from "../_api/ApiInstance";
 
 const Comment = () => {
-    const comments = [
-        {
-            username: 'rani',
-            body: 'wah anaknya udah merangkak? gimana kabarnya fatin?'
-        },
-        {
-            username: 'ciam',
-            body: 'fatin udah besaaar'
-        },
-        {
-            username: 'yudin',
-            body: 'Sudah gede nakenku'
-        },
-    ]
+    const urlParams = useParams();
+    const [comments, setComments] = useState([]);
+
+    const fetchComments = async () => {
+        const {id} = urlParams;
+        const params = {
+            'quote_id': id
+        }
+        const resp = await Api.get('/comment', {params: params});
+        const data = resp.data;
+        const {result} = data;
+        setComments(result);
+    }
+
+    useEffect(() => {
+        fetchComments();
+    }, []);
     return ( 
         <div className="comment">
             {comments.map((item, index) => (
@@ -27,7 +33,7 @@ const Comment = () => {
                     </div>
                     <div>
                         <div>
-                            <b>{item.username}</b> {item.body}
+                            <b>{item.authorId?.username}</b> {item.body}
                         </div>
                         <div className='comment-action'>
                             <span>Reply</span>
