5b2f831d4e3e3f3f6ff9ec6cdf359da76a68c1e0
haerulmutakin <haerulm.takin@gmail.com>
2022-07-13 13:42:22 +0700

    using populate query

--- server/controllers/quote.controller.js
+++ server/controllers/quote.controller.js
@@ -2,6 +2,17 @@ const ResponseSchema = require('../_utils/response-body.util');
 const Quote = require('../models/quote.model');
 
 exports.findQuotes = async (req, res) => {
-    const quotes = await Quote.find();
-    res.send(ResponseSchema.list(quotes))
+    const quotes = await Quote.find().populate('userId', 'username');
+    const result = [];
+    quotes.forEach(item => {
+        const {_id, username } = item.userId;
+        result.push({
+            "_id": item._id,
+            "userId": _id,
+            "username": username,
+            "theme": item.theme,
+            "body": item.body
+        })
+    });
+    res.send(ResponseSchema.list(result))
 }
--- server/models/quote.model.js
+++ server/models/quote.model.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const User = require('./user.model');
 
 const schema = new mongoose.Schema({
-    userId: String,
-    username: String,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    },
     theme: String,
     body: String
 })
