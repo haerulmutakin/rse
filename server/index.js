fd031a249cfbc71cb4d401f6d4c09abf972c184f
haerulmutakin <haerulm.takin@gmail.com>
2022-07-13 15:10:33 +0700

    update server index

--- server/index.js
+++ server/index.js
@@ -1,54 +1,33 @@
 const express = require('express');
-const http = require('http');
-const {Server} = require('socket.io');
+const mongoose = require('mongoose');
 const cors = require('cors');
-
+const parser = require('body-parser');
+const appRoutes = require('./routes')
 
 const App = express();
-App.use(cors())
 
-const httpServer = http.createServer(App);
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 
-const socketIo = new Server(httpServer, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
-});
+App.use(cors(corsOptions));
+App.use(parser.json());
+App.use(parser.urlencoded({extended: true}));
 
-const onlineUsers = [];
+App.use('/api/v1', appRoutes)
 
-const addOnlineUser = (username, socketId) => {
-    const userIndex = onlineUsers.findIndex(item => item.username === username);
-    if(userIndex >= 0) {
-        onlineUsers[userIndex] = {username: username, socketId: socketId}
+mongoose.connect('mongodb://localhost:27017/react-socket-node', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if(err) {
+        console.log(err)
     } else {
-        onlineUsers.push({username: username, socketId: socketId})
+        console.log('Successfully connected to mongodb')
     }
-}
-
-const getUser = (username) => {
-    return onlineUsers.find(item => item.username === username)
-}
-
-socketIo.on('connection', (socket) => {
-
-    socket.on('newOnlineUser', (data) => {
-        addOnlineUser(data.username, socket.id);
-    })
-
-    socket.on('like', ({receiver, sender}) => {
-        const user = getUser(receiver);
-        if(user) {
-            socket.to(user.socketId).emit('like', `${sender} likes your quote`)
-        }
-    })
-
-    socket.on('sm', (data) => {
-        socket.to(data.room).emit('rm', data)
-    })
 })
 
-httpServer.listen(3001, () => {
-    console.log('Server is running')
+App.listen(3001, () => {
+    console.log('App is running')
 })
