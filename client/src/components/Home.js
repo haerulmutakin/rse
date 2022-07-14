913b6e77ad355321dbba485e2aa9b72b586ca603
haerulmutakin <haerulm.takin@gmail.com>
2022-07-14 11:40:35 +0700

    set online users

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import Card from "./Card";
 import Api from "../_api/ApiInstance";
 import { useNavigate } from "react-router-dom";
+import SocketContext from "../_context/SocketContext";
 
 const Home = () => {
+    const {onlineUsers} = useContext(SocketContext);
     const navigate = useNavigate();
     const [ quotes, setQuotes ] = useState([])
 
@@ -23,6 +25,18 @@ const Home = () => {
     useEffect(() => {
         fetchQuotes();
     }, [])
+
+    useEffect(() => {
+        const q = [...quotes];
+        q.forEach(item => {
+            item.online = false;
+            if(onlineUsers.includes(item.userId)) {
+                item.online = true;
+            }
+        })
+        setQuotes(q)
+    }, [onlineUsers])
+
     return ( 
         <div className="home">
             <div className="card-container">
