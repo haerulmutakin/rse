4f1f6c6572115607abadbc6c9bb4eb20d9d0921c
haerulmutakin <haerulm.takin@gmail.com>
2022-07-15 21:42:06 +0700

    specify detail title

--- client/src/components/layout/Detail.js
+++ client/src/components/layout/Detail.js
@@ -1,4 +1,4 @@
-import { useNavigate, useOutlet } from 'react-router-dom';
+import { useNavigate, useOutlet, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { Fragment } from 'react';
@@ -6,6 +6,10 @@ import { Fragment } from 'react';
 const Detail = () => {
     const navigate = useNavigate();
     const outlet = useOutlet();
+    const urlparams = useParams();
+
+    const splitedParams = urlparams['*'].split('/')
+    const secondParams = splitedParams[1].replace('-', ' ');
     
     const handleBack = () => {
         navigate(-1);
@@ -14,7 +18,7 @@ const Detail = () => {
         <Fragment>
             <div className="detail-navbar d-flex align-center">
                 <div onClick={handleBack} className='arrow-back mr-10'><FontAwesomeIcon icon={faArrowLeft} /></div>
-                <div>Activities</div>
+                <div className='detail-layout-title'>{secondParams}</div>
             </div>
             {outlet}
         </Fragment>
