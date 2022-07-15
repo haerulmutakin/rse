8d6fa1cb1cb65767e355b15cc9801ffbb6cb65ce
haerulmutakin <haerulm.takin@gmail.com>
2022-07-15 21:26:35 +0700

    update notification ui

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,8 +1,8 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-
 import SocketContext from "../_context/SocketContext";
+import { format } from '../_helpers/Date';
 
 const Notification = () => {
     const {onlineUsers, notifications} = useContext(SocketContext);
@@ -11,13 +11,16 @@ const Notification = () => {
         return onlineUsers.includes(userId)
     }
     return ( 
-        <div className="mx-8">
+        <div className="notification">
             {notifications.map(item => (
-                <div key={item._id} className="d-flex align-center my-10">
+                <div key={item._id} className="d-flex align-center  mx-5 my-10">
                      <div className={`profile profile-3 ${isOnline(item?.authorId._id) ? 'online' : ''}`}>
                         <FontAwesomeIcon icon={faUser} />
                     </div>
-                    <div className="user-name"><b>{item?.authorId.username}</b> {item.type} your quote</div>
+                    <div>
+                        <span className="sender-name mr-3">{item?.authorId.username}</span>
+                        <span className="notif-desc">{item?.type === 'like' ? 'liked your quote' : `commented: ${item.body}`} {format(item.createdAt, 'DD MMMM')}</span>
+                    </div>
                 </div>
             ))}
         </div>
