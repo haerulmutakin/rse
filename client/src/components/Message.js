8d841d67fcdd3ff3c560fc4efad7e1a9da833d53
haerulmutakin <haerulm.takin@gmail.com>
2022-07-16 22:58:01 +0700

    get rooms from context

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,6 +1,17 @@
+import {useContext } from 'react';
+// import UserContext from '../_context/UserContext';
+import SocketContext from '../_context/SocketContext';
+import MessageItem from './MessageItem';
+
 const Message = () => {
+    // const user = useContext(UserContext);
+    const {userRooms} = useContext(SocketContext)
     return ( 
-        <div>Message</div>
+        <div>
+            {userRooms.map(item => (
+                <MessageItem key={item._id} item={item} />
+            ))}
+        </div>
      );
 }
  
