4792a2ce3623a46857f217d66f76c61752c66fae
haerulmutakin <haerulm.takin@gmail.com>
2022-07-16 22:58:11 +0700

    init message item

---
+++ client/src/components/MessageItem.js
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+const MessageItem = ({item}) => {
+    console.log(item)
+    const navigate = useNavigate();
+
+    const handleRoomClick = () => {
+        navigate(`/custom/room/${item._id}`)
+    }
+    return ( 
+        <div onClick={handleRoomClick} key={item._id} className="chat-room d-flex align-center p-5">
+            <div className={`profile profile-3`}>
+                <FontAwesomeIcon icon={faUser} />
+            </div>
+            <div>
+                {`${item?.receiver?.firstName} ${item?.receiver?.lastName}`}
+            </div>
+        </div>
+     );
+}
+ 
+export default MessageItem;
