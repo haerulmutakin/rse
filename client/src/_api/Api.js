746e903b2b901a6941b452fd70dcca5537bd128c
haerulmutakin <haerulm.takin@gmail.com>
2022-07-25 22:19:49 +0700

    update storage content

--- client/src/_api/Api.js
+++ client/src/_api/Api.js
@@ -17,7 +17,7 @@ const login = async (username) => {
     const resp = await Api.get('/user', {params: {username}});
     const {result} = resp;
     if(result) {
-        localStorage.setItem('session', JSON.stringify({result}));
+        localStorage.setItem('session', JSON.stringify(result));
         return true;
     }
     return false;
