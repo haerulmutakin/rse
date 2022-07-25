f03758e15c92a288d7bb1b57c89c378dd18c3509
haerulmutakin <haerulm.takin@gmail.com>
2022-07-25 21:59:25 +0700

    add response interceptors

--- client/src/_api/Api.js
+++ client/src/_api/Api.js
@@ -1,12 +1,26 @@
-// import Axios from 'axios';
+import Axios from 'axios';
 
-// const Api = Axios.create({
-//     baseURL: "http://localhost:3001/api/v1"
-// });
+const Api = Axios.create({
+    baseURL: "http://localhost:3001/api/v1"
+});
+
+Api.interceptors.response.use(
+    (response) => {
+        return response.data;
+    },
+    (error) => {
+        console.log(error)
+    }
+)
 
 const login = async (username) => {
-    localStorage.setItem('session', JSON.stringify({username}))
-    return true;
+    const resp = await Api.get('/user', {params: {username}});
+    const {result} = resp;
+    if(result) {
+        localStorage.setItem('session', JSON.stringify({result}));
+        return true;
+    }
+    return false;
 }
 
 export {login};
