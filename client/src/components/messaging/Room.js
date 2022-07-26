de510b66019cb09a46e71de2354be4a3b4c217f4
haerulmutakin <haerulm.takin@gmail.com>
2022-07-26 09:23:47 +0700

    init room

---
+++ client/src/components/messaging/Room.js
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { format } from "_helpers/Date";
+import ProfilePlaceholder from "components/common/ProfilePlaceholder";
+
+const Room = ({room}) => {
+    const navigate = useNavigate();
+
+    const getReceiver = () => {
+        let receiverLabel = '';
+        const {receiver} = room;
+        receiver.forEach(item => {
+            receiverLabel += `${item.firstName} ${item.lastName}`
+        })
+        return receiverLabel;
+    }
+    const handleNavigate = () => {
+        navigate('/room')
+    }
+    return ( 
+        <div className="room" onClick={handleNavigate}>
+            <ProfilePlaceholder />
+            <div className="room-info">
+                <div className="username">{getReceiver()}</div>
+                <div className="message">{room.last_message?.body}</div>
+            </div>
+            <div className="time">{format(room.updatedAt, 'D/M/YY')}</div>
+        </div>
+    );
+}
+ 
+export default Room;
