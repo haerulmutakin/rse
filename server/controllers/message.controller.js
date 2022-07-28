63ede0821de7ad0d81ca66f288c094841d5bf970
haerulmutakin <haerulm.takin@gmail.com>
2022-07-28 13:15:54 +0700

    get new message before emit back

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -3,7 +3,7 @@ const Message = require('../models/message.model');
 const Room = require('../models/room.model');
 
 findLastMessageByRoomId = async (roomId) => {
-    const messageData = await Message.findOne({room: roomId}).select('_id, sender, body').sort({'createdAt': 'desc'}).populate('sender');
+    const messageData = await Message.find({room: roomId}).sort({'createdAt': 'asc'}).populate('sender');
     return messageData;
 }
 
@@ -18,9 +18,10 @@ addMessage = async (socket, data) => {
     await messageData.save();
 
     await Room.findOneAndUpdate({_id: messageData.room}, {updated_at: Date.now()});
-    // socket.emit('new', messageData);
-    socket.to(messageData.room).emit('new:roommessage', messageData)
-    console.log('emiting')
+
+    const newMessage = await Message.findOne({_id: messageData._id}).populate('sender');
+
+    socket.to(messageData.room).emit('new:roommessage', newMessage);
 
 }
 
