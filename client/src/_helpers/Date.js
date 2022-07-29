c5c3c9dd1a7ad24897c6945e80cc305af66abc27
haerulmutakin <haerulm.takin@gmail.com>
2022-07-29 14:34:11 +0700

    date

--- client/src/_helpers/Date.js
+++ client/src/_helpers/Date.js
@@ -1,6 +1,9 @@
 import dayjs from "dayjs"
 
 const format = (dateStr, format = 'DD-MM-YYYY') => {
+    if(!dateStr) {
+        return ''
+    }
     const date = new Date(dateStr);
     const tempDate = dayjs(date);
     return tempDate.format(format)
