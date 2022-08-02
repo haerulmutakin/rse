f2aa0995a64a12544b81a045f4f5222e59030eea
haerulmutakin <haerulm.takin@gmail.com>
2022-08-02 09:10:42 +0700

    return only rooms that have messages

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -20,14 +20,15 @@ findUserRooms = async (req, res) => {
         const receivers = roomMembers.filter(item => item.id !== user_id);
 
         const messages = await getMessages(room.id);
-        
-        payload.push({
-            id: room.id,
-            receiver: receivers,
-            messages: messages,
-            created_at: room.created_at,
-            updated_at: room.updated_at
-        })
+        if(messages.length > 0) {
+            payload.push({
+                id: room.id,
+                receiver: receivers,
+                messages: messages,
+                created_at: room.created_at,
+                updated_at: room.updated_at
+            })
+        }
     }
 
     res.send(ResponseSchema.list(payload))
